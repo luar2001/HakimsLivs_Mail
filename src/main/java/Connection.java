58637950c1224d7007525e3ec67c4b4fcf34bfd6
lukas <Lukas.aronsson@protonmail.com>
2021-09-13 09:16:25 +0200

    Created the Connection.java class using OkHttpClient

---
+++ src/main/java/Connection.java
@@ -0,0 +1,51 @@
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * Http Request
+ */
+ class Connection {
+
+    /**
+     * OkHttp object
+     */
+    private static final OkHttpClient client = new OkHttpClient().newBuilder()
+            .build();
+
+    /**
+     * HTTP Request using okHttp
+     * @param url Http Request url
+     * @param method Request method | examples: "POST" or "GET"
+     * @param json sends as a response with the request
+     * @param headers the curl headers
+     * @return String with the response from the request.
+     */
+    @NotNull
+     static String httpRequest(String url, String method, String json, Headers headers){
+
+        RequestBody body = RequestBody.create(json, MediaType.parse("application/json;charset=UTF-8"));
+
+        assert false;
+        Request request = new Request.Builder()
+                .url(url)
+                .method(method,body)
+                .headers(headers)
+                .build();
+
+        try(Response response = client.newCall(request).execute()){
+
+            if(!response.isSuccessful()) throw new IOException("\nConnection ERROR: \n" + response);
+
+            return Objects.requireNonNull(response.body()).string();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "\nHTTP Request ERROR: \n";
+        }
+
+    }
+
+}
