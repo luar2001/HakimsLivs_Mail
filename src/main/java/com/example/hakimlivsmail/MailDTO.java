8bce7e67c705999d5fcfc1dd7dc8a5cfd20cb9bc
kdhedman <kenth.david.hedman@gmail.com>
2021-09-14 10:22:24 +0200

    Small changes..

--- src/main/java/com/example/hakimlivsmail/MailDTO.java
+++ src/main/java/com/example/hakimlivsmail/MailDTO.java
@@ -1,4 +1,18 @@
 package com.example.hakimlivsmail;
 
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class MailDTO {
+    @JsonProperty
+    private String toMail;
+    @JsonProperty
+    private String subject;
+    @JsonProperty
+    private String content;
+
+    @JsonCreator
+
+
 }
